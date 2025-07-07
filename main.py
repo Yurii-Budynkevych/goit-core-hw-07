59d7af8361169236afd5c47d2781b0689d97d70c
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2025-07-07 21:01:33 +0200

    "refactored"

--- main.py
+++ main.py
@@ -5,6 +5,80 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+def add_handler(args, book):
+    try:
+        if not args:
+            raise Exception('Please enter a name.')
+        record = book.find(args[0])
+        if record is not None:
+            record.add_phone(args[1])
+        else:
+            book.add_record(Record(*args))
+    except Exception as e:
+        print("Error occurred while adding contact:", e)
+
+def change_handler(args, book):
+    try:
+        if not args:
+            raise Exception('Please enter a name.')
+        record = book.find(args[0])
+        if record is not None:
+            record.edit_phone(args[1], args[2])
+        else:
+            print(f"Record with name {args[0]} not found")
+    except Exception as e:
+        print("Error occurred while changing phone number:", e)
+
+def phone_handler(args, book):
+    try:
+        if not args:
+            raise Exception('Please enter a name.')
+        record = book.find(args[0])
+        if record is not None:
+            print(record.get_phones())
+        else:
+            print(f"Record with name {args[0]} not found")
+    except Exception as e:
+        print("Error occurred while showing phone:", e)
+
+def add_birthday_handler(args, book):
+    try:
+        if not args:
+            raise Exception('Please enter a name.')
+        record = book.find(args[0])
+        if record is not None:
+            record.add_birthday(args[1])
+        else:
+            print(f"Record with name {args[0]} not found")
+    except Exception as e:
+        print("Error occurred while adding birthday:", e)
+
+def show_birthday_handler(args, book):
+    try:
+        if not args:
+            raise Exception('Please enter a name.')
+        record = book.find(args[0])
+        if record is not None:
+            print(record.get_birthday())
+        else:
+            print(f"Record with name {args[0]} not found")
+    except Exception as e:
+        print("Error occurred while showing birthday:", e)
+
+def birthdays_handler(args, book):
+    try:
+        print(book.show_records_birthdays())
+    except Exception as e:
+        print("Error occurred while listing birthdays:", e)  
+
+def all_handler(args, book):
+    if not book.data:
+        print("No contacts available.")
+    else:
+        for record in book.values():
+            print(record)                                      
+
+
 def main():
     book = AddressBook()
     print("Welcome to the assistant bot!")
@@ -20,77 +94,25 @@ def main():
             print("How can I help you?")
 
         elif command == "add":
-            try:
-                if not args:
-                    raise Exception('Please enter a name.')
-                record = book.find(args[0])
-                if record is not None:
-                    record.add_phone(args[1])
-                else:
-                    book.add_record(Record(*args))
-            except Exception as e:
-                print("Error occurred while adding contact:", e)
+            add_handler(args, book)
 
         elif command == "change":
-            try:
-                if not args:
-                    raise Exception('Please enter a name.')
-                record = book.find(args[0])
-                if record is not None:
-                    record.edit_phone(args[1], args[2])
-                else:
-                    print(f"Record with name {args[0]} not found")
-            except Exception as e:
-                print("Error occurred while changing phone number:", e)
+            change_handler(args, book)
 
         elif command == "phone":
-            try:
-                if not args:
-                    raise Exception('Please enter a name.')
-                record = book.find(args[0])
-                if record is not None:
-                    print(record.get_phones())
-                else:
-                    print(f"Record with name {args[0]} not found")
-            except Exception as e:
-                print("Error occurred while showing phone:", e)
+            phone_handler(args, book)
 
         elif command == "add-birthday":
-            try:
-                if not args:
-                    raise Exception('Please enter a name.')
-                record = book.find(args[0])
-                if record is not None:
-                    record.add_birthday(args[1])
-                else:
-                    print(f"Record with name {args[0]} not found")
-            except Exception as e:
-                print("Error occurred while adding birthday:", e)
+            add_birthday_handler(args, book)
 
         elif command == "show-birthday":
-            try:
-                if not args:
-                    raise Exception('Please enter a name.')
-                record = book.find(args[0])
-                if record is not None:
-                    print(record.get_birthday())
-                else:
-                    print(f"Record with name {args[0]} not found")
-            except Exception as e:
-                print("Error occurred while showing birthday:", e)
+            show_birthday_handler(args, book)
 
         elif command == "birthdays":
-            try:
-                print(book.show_records_birthdays())
-            except Exception as e:
-                print("Error occurred while listing birthdays:", e)
+            birthdays_handler(args, book)
 
         elif command == "all":
-            if not book.data:
-                print("No contacts available.")
-            else:
-                for record in book.values():
-                    print(record)
+            all_handler(args, book)
 
         else:
             print("Invalid command.")
